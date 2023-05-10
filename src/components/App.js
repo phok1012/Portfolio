be5bf43fb8da6e1fa62589d9772062190fea49e4
Kevin <KevinPho1012@gmail.com>
2023-05-10 12:47:36 -0700

    saving layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import Projects from './Projects';
+import Contact from './Contact';
+
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello worlds!</h1>
-        </div>
-    );
+  const [page, setPage] = useState('Home')
+
+  return (
+    <div>
+      <NavBar setPage={setPage} />
+      <div>
+      {
+        {
+          'Home': <Home />,
+          'Projects': <Projects />,
+          'Contact':<Contact />
+        }[page]
+      }
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,23 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                        }
-                }
+              test: /\.css$/,
+              use: {
+                loader:[
+                  'style-loader',
+                  'css-loader'
+                ]
+              }
+            },
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['@babel/preset-env', '@babel/preset-react']
+                      }
+              }
             }
         ]
     }
