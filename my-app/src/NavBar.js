be27f6968492940262ad4841664da5926ca64f3f
Kevin <KevinPho1012@gmail.com>
2023-05-10 07:38:35 -0700

    cleaned up boiler plate

---
+++ my-app/src/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className='NavBar'>Picture Name about projects contact </div>
+  )
+}
+
+export default NavBar
